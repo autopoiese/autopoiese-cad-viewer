e00fbd36b81c8a57823c3a47452c5b07adf791fd
jfa <git@jonathanallenberg.de>
2021-05-11 15:19:42 +0200

    utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,3 +11,32 @@ export function isPerpectiveCamera(
 ): def is THREE.PerspectiveCamera {
   return (def as THREE.PerspectiveCamera).isPerspectiveCamera
 }
+
+export const getDiameter = (
+  object: THREE.Object3D | THREE.BufferGeometry | THREE.Box3
+) => {
+  let radius
+  if (object instanceof THREE.BufferGeometry) {
+    object.computeBoundingSphere()
+    radius = (object.boundingSphere as THREE.Sphere).radius
+  } else if (object instanceof THREE.Box3) {
+    const boundingSphere = new THREE.Sphere()
+    object.getBoundingSphere(boundingSphere)
+    radius = boundingSphere.radius
+  } else {
+    radius = getBoundingSphere(object).radius
+  }
+  if (radius !== Infinity) {
+    return radius * 2
+  }
+  return 0
+}
+
+export const getBoundingBox = (object: THREE.Object3D): THREE.Box3 =>
+  new THREE.Box3().setFromObject(object)
+
+export const getBoundingSphere = (object: THREE.Object3D): THREE.Sphere => {
+  const sphere = new THREE.Sphere()
+  getBoundingBox(object).getBoundingSphere(sphere)
+  return sphere
+}
