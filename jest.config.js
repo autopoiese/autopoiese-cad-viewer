e6f58f280797681933367db67b54a77c905f3165
Jonathan Allenberg <git@jfa.sh>
2022-06-13 14:13:26 +0200

    ...

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'cad-viewer',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/cad/viewer'
+}
